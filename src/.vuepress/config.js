8421d5a09205d5b99637f2a049ba1c04f65eeb31
Max Chou <yihsunmaxchou@icloud.com>
2021-05-29 11:08:45 +0800

    Project Init

---
+++ src/.vuepress/config.js
@@ -0,0 +1,66 @@
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'MineNTU 臺大麥塊',
+  description: 'MineNTU 是專屬於臺大生的 Minecraft 伺服器，由一群臺大學生創立、管理。目前有一個創造伺服器，大家都在裡面蓋臺大。',
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    smoothScroll: true,
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    logo: '/images/minentu-logo.png',
+    nav: [
+      {
+        text: '公告與守則',
+        link: '/terms-and-conditions/',
+      },
+      {
+        text: '申請規則',
+        link: '/requests/'
+      },
+      {
+        text: '送我們一些椰子 🥥',
+        link: 'https://paypal.me/maxchou'
+      },
+      {
+        text: '作者群',
+        link: '/team/'
+      },
+      {
+        text: '我要加入 🐵',
+        link: 'https://forms.gle/XSyaXqTk6knEvFpUA'
+      }
+    ],
+    sidebar: {
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+    '@vuepress/nprogress'
+  ]
+}
---
+++ src/index.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroImage: /images/minentu-banner.png
+tagline: null
+actionText: 馬上開始線上猴園生活！
+actionLink: /how-to-join/
+footer: Made by 🐵 (Team MineNTU) with 🌴 🥥 .
+---
+
+**⚠️ 因目前伺服器資源有限，目前只開放國立臺灣大學員生（包含中研院於臺大）及校友註冊。**
+
+# Introduction 🚲
+**MineNTU** 是專屬於臺大生的 Minecraft 伺服器，由一群 **~~沒事做的臺大猴~~** 臺大學生創立、管理。目前有一個創造伺服器，大家都在裡面蓋臺大。
+
